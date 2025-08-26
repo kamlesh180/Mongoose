1b656a788ffdb6d847d4c2d939d9d00bb676e41c
kamlesh <kamlesh81473@gmail.com>
2025-08-26 21:51:30 +0530

    add new code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
+const chat=require("./models/chat.js");
+
+app.set("view",path.join(__dirname,"views"));
+app.set("views engine","ejs");
 
 main ()
 .then (()=> {
@@ -10,8 +15,22 @@ main ()
     console.log(err);
 });
 async function main(){
-await mongoose.connect('mongodb://127.0.0.1:27017/whatsApp');
+await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
+let chat1 = new chat({
+    from:"kamlesh",
+    to :"rahul",
+    msg:"hey rahul , can you solve this problem",
+    created_at:new Date(),
+});
+chat1.save()
+.then(res =>{
+    console.log(res);
+})
+.catch(err =>{
+    console.log(err);
+});
+
 app.get("/",(req,res) => {
     res.send("root is working");
 });
